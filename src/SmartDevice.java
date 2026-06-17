789f0d3ce616255e9836130ab5945e2c1bf29c7e
MCBilder <norbert.golyga@vp.pl>
2026-06-17 23:45:58 +0200

    Zadanie 2(Podstawy OOP i Hermetyzacja)

---
+++ src/SmartDevice.java
@@ -0,0 +1,27 @@
+public class SmartDevice {
+    private String id;
+    private String name;
+    private String room;
+    private String macAddress;
+    private double firmwareVersion;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public String getMacAddress() {
+        return macAddress;
+    }
+
+    public double getFirmwareVersion() {
+        return firmwareVersion;
+    }
+}
